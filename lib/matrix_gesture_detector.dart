5de5a6b368e6eae5099a87dea21d65c86bbec119
Rifqi Rahmad Ichsan Makayasa Pratama <52215902+makayasa@users.noreply.github.com>
2022-10-20 00:17:07 +0700

    Update matrix_gesture_detector.dart

--- lib/matrix_gesture_detector.dart
+++ lib/matrix_gesture_detector.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'dart:developer' as dev;
 import 'dart:convert';
 
-
 import 'package:flutter/widgets.dart';
 import 'package:vector_math/vector_math_64.dart';
 
@@ -144,27 +143,27 @@ class _MatrixGestureDetectorState extends State<MatrixGestureDetector> {
     onUpdate: (oldVal, newVal) => newVal - oldVal,
   );
 
-  void logKey([key, content]) {
-    String finalLog = '';
-    dynamic tempContent = content ?? key;
-    if (tempContent is Map || tempContent is List) {
-      try {
-        finalLog = json.encode(tempContent);
-      } catch (e) {
-        finalLog = tempContent.toString();
-      }
-    } else if (tempContent is String) {
-      finalLog = tempContent;
-    } else {
-      finalLog = tempContent.toString();
-    }
+  // void logKey([key, content]) {
+  //   String finalLog = '';
+  //   dynamic tempContent = content ?? key;
+  //   if (tempContent is Map || tempContent is List) {
+  //     try {
+  //       finalLog = json.encode(tempContent);
+  //     } catch (e) {
+  //       finalLog = tempContent.toString();
+  //     }
+  //   } else if (tempContent is String) {
+  //     finalLog = tempContent;
+  //   } else {
+  //     finalLog = tempContent.toString();
+  //   }
 
-    if (content != null) {
-      dev.log('$key => $finalLog');
-    } else {
-      dev.log(finalLog);
-    }
-  }
+  //   if (content != null) {
+  //     dev.log('$key => $finalLog');
+  //   } else {
+  //     dev.log(finalLog);
+  //   }
+  // }
 
   void onScaleStart(ScaleStartDetails details) {
     translationUpdater.value = details.focalPoint;
@@ -207,8 +206,8 @@ class _MatrixGestureDetectorState extends State<MatrixGestureDetector> {
     // handle matrix translating
     if (widget.shouldTranslate) {
       Offset translationDelta = translationUpdater.update(details.focalPoint);
-      logKey('dx', translationDelta.dx);
-      logKey('dy', translationDelta.dy);
+      // logKey('dx', translationDelta.dx);
+      // logKey('dy', translationDelta.dy);
       testOffset = translationDelta;
       translationDeltaMatrix = _translate(translationDelta);
       matrix = translationDeltaMatrix * matrix;
@@ -220,7 +219,7 @@ class _MatrixGestureDetectorState extends State<MatrixGestureDetector> {
     // handle matrix scaling
     if (widget.shouldScale && details.scale != 1.0) {
       double scaleDelta = scaleUpdater.update(details.scale);
-      logKey('scaleDelta', scaleDelta);
+      // logKey('scaleDelta', scaleDelta);
       testScale = scaleDelta;
       scaleDeltaMatrix = _scale(scaleDelta, focalPoint);
       matrix = scaleDeltaMatrix * matrix;
@@ -229,7 +228,7 @@ class _MatrixGestureDetectorState extends State<MatrixGestureDetector> {
     // handle matrix rotating
     if (widget.shouldRotate && details.rotation != 0.0) {
       double rotationDelta = rotationUpdater.update(details.rotation);
-      logKey('rotationDelta',rotationDelta);
+      // logKey('rotationDelta',rotationDelta);
       testRotation = rotationDelta;
       rotationDeltaMatrix = _rotate(rotationDelta, focalPoint);
       matrix = rotationDeltaMatrix * matrix;
